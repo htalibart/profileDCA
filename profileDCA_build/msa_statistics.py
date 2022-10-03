83b9200a30d030317a3ae7ca2d2be603cacdd14c
Hugo Talibart <htalibart@debian-BULLSEYE-live-builder-AMD64>
2022-10-03 13:04:27 +0200

    count letter only if letter is in alphabet

--- profileDCA_build/msa_statistics.py
+++ profileDCA_build/msa_statistics.py
@@ -23,7 +23,8 @@ def compute_double_frequencies(msa, alphabet_dict=ALPHABET_DICT):
     for i in range(L):
         for j in range(i,L):
             for n in range(N):
-                double_counts[i,j,msa[n,i],msa[n,j]]+=1
+                if (msa[n,i] in alphabet_dict) and (msa[n,j] in alphabet_dict):
+                    double_counts[i,j,msa[n,i],msa[n,j]]+=1
             double_counts[j,i] = np.transpose(double_counts[i,j])
     double_f = double_counts/N
     return double_f
